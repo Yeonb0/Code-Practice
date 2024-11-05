d0fb433e8c2068f7ea6a141508646670927abcc7
Boyeon Kim <154862473+Yeonb0@users.noreply.github.com>
2024-11-05 10:04:35 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n, m;
+	
+	// 입력
+	cin >> n >> m;
+	
+	// 출력
+	cout << (n*m) - 1;
+	
+	return 0;
+}
