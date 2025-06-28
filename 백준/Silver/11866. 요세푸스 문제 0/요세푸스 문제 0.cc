ff43b92753f182119b11f785cf45c516b660b67f
Boyeon Kim <154862473+Yeonb0@users.noreply.github.com>
2025-06-28 17:49:15 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <queue>
+#include <list>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int n, k;
+	cin >> n >> k;
+	list<int> answer;
+	queue<int> people;
+	
+	// 큐에 원소 넣기
+	for(int i = 0; i < n; ) {
+		people.push(++i);
+	}
+	
+	int count = 1;
+	// 큐가 빌 때 까지
+	while(!people.empty()) {
+		if (count == k) {
+			int plus = people.front();
+			people.pop();
+			answer.push_back(plus);
+			count = 1;
+		} else {
+			count++;
+			int pop = people.front();
+			people.pop();
+			people.push(pop);
+		}
+	}
+		
+	// 출력 부분 수정
+  cout << "<";
+  for (auto it = answer.begin(); it != answer.end(); it++) {
+      if (it != answer.begin()) cout << ", ";
+      cout << *it;
+  }
+  cout << ">\n";
+
+  return 0;
+}
