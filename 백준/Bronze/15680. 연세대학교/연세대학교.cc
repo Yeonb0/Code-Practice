d7ce98b79a163f6dda78c651396dee8bc9ba76a9
Boyeon Kim <154862473+Yeonb0@users.noreply.github.com>
2025-06-29 23:48:03 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	if (n == 1) cout << "Leading the Way to the Future";
+	else cout << "YONSEI";
+	return 0;
+}
