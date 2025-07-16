bbe02196d6d1f126785ecb3181d0945e6c251bda
Boyeon Kim <154862473+Yeonb0@users.noreply.github.com>
2025-07-16 13:00:54 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int ans[6] = {1, 1, 2, 2, 2, 8};
+	for(int i = 0; i < 6; i++) {
+		int a;
+		cin >> a;
+		cout << ans[i] - a << " ";
+	}
+	return 0;
+}
