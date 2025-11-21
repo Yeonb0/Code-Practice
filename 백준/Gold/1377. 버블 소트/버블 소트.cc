81c0353c6b6caf20f20947fb14936efd4ab1aeaa
Boyeon Kim <154862473+Yeonb0@users.noreply.github.com>
2025-11-21 15:16:18 +0900

    [Gold II] Title: 버블 소트, Time: 92 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Gold/1377. 버블 소트/버블 소트.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int n;
+	cin >> n;
+	vector<pair<int, int>> v(n);
+	for(int i = 0; i < n; i++) {
+		cin >> v[i].first;
+		v[i].second = i;
+	}
+	sort(v.begin(), v.end());
+	int max = 0;
+	
+	for(int i = 0; i < n; i++) {
+		if(max < v[i].second - i) {
+			max = v[i].second - i;
+		}
+	}
+	cout << max + 1;
+	return 0;
+}
