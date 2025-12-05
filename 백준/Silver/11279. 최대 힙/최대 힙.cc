1b794c21439ed528e214844346c048f5397d1c5c
Boyeon Kim <154862473+Yeonb0@users.noreply.github.com>
2025-12-05 20:42:25 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+priority_queue<int> pq;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int n;
+	cin >> n;
+	
+	while(n--) {
+		int x;
+		cin >> x;
+		if(x > 0) pq.push(x);
+		else {
+			if(!pq.empty()) {
+				cout << pq.top() << "\n";
+				pq.pop();
+			} else cout << "0\n";
+		}
+	}
+	return 0;
+}
